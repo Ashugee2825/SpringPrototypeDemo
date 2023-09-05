4899b9afb9d20f3337c82aa567d807b13510b723
Ashutosh Kumar <66332175+Ashugee2825@users.noreply.github.com>
2023-09-05 16:07:48 +0530

    Add files via upload

---
+++ SpringprotypeDemo/src/main/java/com/telusko/SpringprotypeDemo/Alien.java
@@ -0,0 +1,25 @@
+package com.telusko.SpringprotypeDemo;
+
+public class Alien {
+
+
+int age;
+	
+	public Alien() {
+		System.out.println("Alien Object Created..");	
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		System.out.println("Age Assigned");
+		this.age = age;
+	}
+
+public void code() 
+{
+	System.out.println("Im Coding..");
+}
+
+}
